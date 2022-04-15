225ede89f3d4dd9a7434b64578fd528c22e1be8f
iangamiere <iangamiere2024@u.northwestern.edu>
2022-04-15 18:30:48 -0500

    Commiting my completed lab03 files

---
+++ lab03/answers.sql
@@ -0,0 +1,57 @@
+-- Exercise 1 (done for you):
+SELECT * FROM users;
+
+-- Exercise 2 (done for you):
+SELECT id, first_name, last_name 
+FROM users;
+
+-- Exercise 3
+SELECT id, first_name, last_name
+FROM users
+ORDER BY last_name;
+
+-- Exercise 4
+SELECT id, image_url, user_id
+FROM posts
+WHERE user_id = 26;
+
+-- Exercise 5
+SELECT id, image_url, user_id
+FROM posts
+WHERE user_id = 26
+OR user_id = 12;
+
+-- Exercise 6
+SELECT COUNT(*) FROM posts;
+
+-- Exercise 7
+SELECT user_id, COUNT(*)
+FROM comments
+GROUP BY user_id
+ORDER BY COUNT(*) desc;
+
+-- Exercise 8
+SELECT posts.id, posts.image_url, posts.user_id, users.username, users.first_name, users.last_name
+FROM posts
+INNER JOIN users ON users.id = posts.user_id
+WHERE posts.user_id = 26
+OR posts.user_id = 12;
+
+-- Exercise 9 (Come back to this)
+SELECT posts.id, posts.pub_date, following.following_id
+FROM posts
+INNER JOIN following ON posts.user_id = following.following_id
+WHERE posts.user_id = 26;
+
+-- Exercise 10
+
+-- Exercise 11 (Come back to this)
+
+-- Exercise 12
+
+-- Exercise 13
+UPDATE users
+SET email = knick2022@gmail.com
+WHERE id = 26;
+
+-- Exercise 14
