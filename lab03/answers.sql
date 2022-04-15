e42b75ae60b1fe58d709aa16fad547f0f7ff44ce
iangamiere <iangamiere2024@u.northwestern.edu>
2022-04-15 18:32:12 -0500

    Commiting my completed lab03 files (again)

--- lab03/answers.sql
+++ lab03/answers.sql
@@ -37,21 +37,40 @@ INNER JOIN users ON users.id = posts.user_id
 WHERE posts.user_id = 26
 OR posts.user_id = 12;
 
--- Exercise 9 (Come back to this)
+-- Exercise 9
 SELECT posts.id, posts.pub_date, following.following_id
 FROM posts
 INNER JOIN following ON posts.user_id = following.following_id
-WHERE posts.user_id = 26;
+WHERE following.user_id = 26;
 
 -- Exercise 10
 
 -- Exercise 11 (Come back to this)
+INSERT INTO bookmarks(user_id, post_id, timestamp)
+VALUES (26, 219, now());
+
+INSERT INTO bookmarks(user_id, post_id, timestamp)
+VALUES (26, 220, now());
+
+INSERT INTO bookmarks(user_id, post_id, timestamp)
+VALUES (26, 221, now());
 
 -- Exercise 12
+DELETE FROM bookmarks
+WHERE user_id = 26
+AND post_id = 219;
+
+DELETE FROM bookmarks
+WHERE user_id = 26
+AND post_id = 220;
+
+DELETE FROM bookmarks
+WHERE user_id = 26
+AND post_id = 221;
 
 -- Exercise 13
 UPDATE users
-SET email = knick2022@gmail.com
+SET email = 'knick2022@gmail.com'
 WHERE id = 26;
 
 -- Exercise 14
